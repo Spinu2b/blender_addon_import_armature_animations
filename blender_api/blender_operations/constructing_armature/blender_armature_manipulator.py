cb176b5e3d68853b63ea06b17cc0805ac17d55bc
Spinu2b <spinu2b@gmail.com>
2019-12-23 22:45:28 +0100

    Creating armature and linking it to the scene in Blender - need to wrap Blender API more (unstable API from version to version, abstract that)

--- blender_api/blender_operations/constructing_armature/blender_armature_manipulator.py
+++ blender_api/blender_operations/constructing_armature/blender_armature_manipulator.py
@@ -4,4 +4,9 @@ from bpy.types import Armature
 
 class BlenderArmatureManipulator:
     def create_armature(self, name: str) -> Armature:
-        return bpy.data.armatures.new(name=name)
+        armature = bpy.data.armatures.new(name=name)
+        armature_obj = bpy.data.objects.new(name + "_OBJECT", armature)
+        bpy.context.collection.objects.link(armature_obj)
+        bpy.context.view_layer.objects.active = armature_obj
+        bpy.context.active_object.select_set(state=True)
+        return armature
