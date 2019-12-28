bf590d7eae97f2f287a56bd63786685728ea03e2
Spinu2b <spinu2b@gmail.com>
2019-12-28 23:10:25 +0100

    Removed unnecessary method

--- utils/model_spaces_integration/math_utils.py
+++ utils/model_spaces_integration/math_utils.py
@@ -3,10 +3,5 @@ from ...utils.model_spaces_integration.vector3d import Vector3d
 
 class MathUtils:
     @classmethod
-    def is_close_enough_to_zero(cls, value: float) -> bool:
-        margin = 0.000001
-        return abs(value) < margin
-
-    @classmethod
     def get_scale_ratio_vector3d(cls, scale_a: Vector3d, scale_b: Vector3d) -> Vector3d:
         raise NotImplementedError
