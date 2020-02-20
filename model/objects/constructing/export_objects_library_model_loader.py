880aaaecdc84648f9cfc0a0a1b4d09bbe6535fc4
Spinu2b <spinu2b@gmail.com>
2020-02-20 19:42:15 +0100

    initial - save just in case

--- model/objects/constructing/export_objects_library_model_loader.py
+++ model/objects/constructing/export_objects_library_model_loader.py
@@ -1,5 +1,7 @@
 import json
 
+import orjson as orjson
+
 from ....model.objects.constructing.export_objects_library_model_constructor import ExportObjectsLibraryModelConstructor
 from ....model.objects.model.export_objects_library_model import ExportObjectsLibraryModel
 
@@ -7,5 +9,8 @@ from ....model.objects.model.export_objects_library_model import ExportObjectsLi
 class ExportObjectsLibraryModelLoader:
     def load(cls, path_to_json_file: str) -> 'ExportObjectsLibraryModel':
         with open(path_to_json_file, 'r') as json_file:
-            json_dict = json.loads(json_file.read())
-        return ExportObjectsLibraryModelConstructor().construct_from_json(json_dict)
+            json_string = json_file.read()  # type: str
+            return orjson.loads(json_string)
+        #with open(path_to_json_file, 'r') as json_file:
+        #    json_dict = json.loads(json_file.read())
+        #return ExportObjectsLibraryModelConstructor().construct_from_json(json_dict)
